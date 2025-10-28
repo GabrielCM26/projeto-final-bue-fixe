687f7f2f10d7b2739309b66c6b4b7dac5e39293a
Paulo Fonseca <39968977+PauloFonseca89@users.noreply.github.com>
2025-10-28 16:20:48 +0000

    update steam btn

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -1,17 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function LoginCard() {
   return (
     <div className="text-center">
       <h1 className="text-2xl font-bold text-lime-400 mb-2">Welcome</h1>
       <h2 className="text-neutral-300 mb-6">"logo" PlayDex</h2>
-      <button
-        className="
-          bg-lime-400 hover:bg-lime-500
-          text-neutral-900 font-semibold
-          py-2 px-4 rounded-lg transition
-        "
-      >
-        Connect Steam
-      </button>
+
+      <Link href="https://steamcommunity.com/openid/login">
+        <Image
+          src="/logoSteam.png"
+          alt="Sign in through Steam"
+          width={100}
+          height={75}
+          className="mx-auto hover:opacity-90 transition"
+        />
+      </Link>
     </div>
   );
 }
