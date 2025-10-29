563118bda2fc74b119af866654a62abaa3993151
Felix <felix.erwin02@gmail.com>
2025-10-29 22:47:17 +0000

    update LoginCard.jsx and index.js

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -2,20 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function LoginCard() {
+  const handleLogin = () => {
+    window.location.href = '/auth/steam';
+  };
+
   return (
-    <div className=" w-[150px] h-[150px] bg-[#0d0f0e] rounded-2xl border border-[#58CE87] shadow-[0_0_15px_rgba(144,238,144,0.1)] flex flex-col items-center justify-center text-center">
-      <h1 className="text-2xl font-bold text-lime-400 mb-2">Welcome</h1>
+    <div className=" w-[150px] h-[150px] bg-[#0d0f0e] rounded-2xl border border-[#58CE87] shadow-[0_0_15px_rgba(144,238,144,0.1)] items-center justify-center text-center">
       <h2 className="text-neutral-400 mb-6">"logo" PlayDex</h2>
 
-      <Link href="https://steamcommunity.com/openid/login">
-        <Image
-          src="/logoSteam.png"
+      <button onClick={handleLogin} className="focus:outline-none">
+        <img
+          src="https://web.archive.org/web/20150227040200im_/http://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_large_noborder.png"
           alt="Sign in through Steam"
-          width={100}
-          height={75}
-          className="mx-auto hover:opacity-90 transition"
+          className="cursor-pointer w-48 hover:opacity-80 transition-opacity duration-200"
         />
-      </Link>
+      </button>
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,15 @@ import LoginCard from "@/components/LoginCard";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen items-center justify-center bg-black overflow-hidden">
-      <div className="bg-[url(./public/Login_BG.png)]">
-
+    <main className="flex min-h-screen items-center justify-center bg-linear-to-t from-[#000d12] from-88% to-[#001334]">
+      <div className="relative">
+        <div className="absolute transform -translate-x-1/2 left-1/2 bottom-1/2">
+         <LoginCard />
+        </div>
+        <img src="/Login_BG.png" />
       </div>
-      <img
-        src="/Login_BG.png"
-        alt="PlayDex wallpaper"
-        fill
-        priority
-        className="object-contain bg-black object-[center_50%] rounded-3xl"
-        
-        sizes="400vw"
-      />
 
       <div className="relative z-10">
-        <LoginCard />
       </div>
     </main>
   );
