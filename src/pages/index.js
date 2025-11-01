2be5045982dd8bf6b806c4564f965f8814bbfeef
Felix <felix.erwin02@gmail.com>
2025-11-01 07:40:06 +0000

    update: fixed bg image responsivity

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="absolute transform -translate-x-1/2 left-1/2 top-80 overflow-hidden shadow-[0_0_8px_rgba(88,206,135,0.5)] rounded-2xl">
          <LoginCard />
         </div>
-        <img src="/Login_BG.png" className="overflow-hidden mt-[5vh] max-w-[440px] md:mt-[1vh]"/>
+        <img src="/Login_BG.png" className="overflow-hidden mt-[70px] max-w-[440px] md:mt-[1vh]"/>
       </div>
     </main>
   );
