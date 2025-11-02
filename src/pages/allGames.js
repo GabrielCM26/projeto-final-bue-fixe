e8472d467075dc037e66680afbc0461ef908b577
Gabriel Moreira <gabrielmoreira7301@gmail.com>
2025-11-02 19:12:48 +0000

    game card template

---
+++ src/pages/allGames.js
@@ -0,0 +1,9 @@
+import TiltCard from "../components/GameCard";
+
+export default function AllGames() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <TiltCard />
+    </div>
+  );
+}
