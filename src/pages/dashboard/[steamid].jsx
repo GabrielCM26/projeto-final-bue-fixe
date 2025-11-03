7a8f459f3ddbbf8c4c9019423392079643b61478
Gabriel Moreira <gabrielmoreira7301@gmail.com>
2025-11-03 00:21:11 +0000

    loading working in dashboard and game page

--- src/pages/dashboard/[steamid].jsx
+++ src/pages/dashboard/[steamid].jsx
@@ -1,12 +1,14 @@
-import { useRouter } from "next/router";
+﻿import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import CardDashboard from "../../components/CardDasboard.jsx";
+import Loader from '@/components/Loader';
 
 export default function Dashboard() {
   const router = useRouter();
   const { steamid } = router.query;
 
   const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [games, setGames] = useState([]);
   const [totalHours, setTotalHours] = useState(0);
   const [bestGame, setBestGame] = useState(null);
@@ -15,6 +17,7 @@ export default function Dashboard() {
     if (!steamid) return;
 
     async function loadData() {
+      setLoading(true);
       try {
         // PERFIL
         const resProfile = await fetch(`/api/profiles/${steamid}`);
@@ -22,10 +25,10 @@ export default function Dashboard() {
 
         const normalizedProfile = dataProfile.userProfile
           ? {
-              ...dataProfile.userProfile,
-              friends: dataProfile.friendsProfiles || [],
-              steamLevel: dataProfile.steamLevel ?? null,
-            }
+            ...dataProfile.userProfile,
+            friends: dataProfile.friendsProfiles || [],
+            steamLevel: dataProfile.steamLevel ?? null,
+          }
           : dataProfile;
 
         setProfile(normalizedProfile);
@@ -71,10 +74,10 @@ export default function Dashboard() {
             }
           }
 
-          
+
           const coverUrl = `https://cdn.akamai.steamstatic.com/steam/apps/${mostPlayed.appid}/library_600x900.jpg`;
 
-       
+
           const achievementsDone = mostPlayed.achievementsDone || 0;
           const achievementsTotal = mostPlayed.achievementsTotal || 0;
 
@@ -82,7 +85,7 @@ export default function Dashboard() {
             appid: mostPlayed.appid,
             name: mostPlayed.name,
             developer: mostPlayed.developer || "",
-            quote: "", 
+            quote: "",
             cover: coverUrl,
             hoursPlayed,
             achievementsDone,
@@ -94,6 +97,8 @@ export default function Dashboard() {
         }
       } catch (error) {
         console.error("Failed to load profile/games:", error);
+      } finally {
+        setLoading(false);
       }
     }
 
@@ -101,10 +106,10 @@ export default function Dashboard() {
   }, [steamid]);
 
   // LOADING STATE
-  if (!profile) {
+  if (loading) {
     return (
       <main className="min-h-screen text-white flex justify-center items-center p-4 bg-[#121212]">
-        <div className="text-gray-500 text-sm">Loading...</div>
+         <Loader />
       </main>
     );
   }
@@ -132,7 +137,7 @@ export default function Dashboard() {
       >
         {/* HEADER */}
         <header className="grid grid-cols-3 items-center">
-         
+
           <div></div>
 
           {/* Logo */}
@@ -144,7 +149,7 @@ export default function Dashboard() {
             />
           </div>
 
-         
+
           <div className="flex items-center gap-2 justify-end">
             <div className="text-sm text-gray-200 truncate max-w-24">
               {profile?.personaname || "user"}
@@ -172,7 +177,7 @@ export default function Dashboard() {
 
         {/* RESTO DO DASHBOARD */}
         <div className="flex flex-col gap-7">
-          
+
           <div className="flex flex-col sm:flex-row gap-2 text-center">
             {/* HOURS PLAYED */}
             <div className="flex-1 bg-[#282828] rounded-md p-3 flex flex-col justify-between transition-all duration-300 hover:shadow-lg">
@@ -221,7 +226,7 @@ export default function Dashboard() {
             </button>
           </div>
 
-        
+
 
           {/* BEST ACHIEVEMENTS */}
           <div className="bg-[#282828] rounded-md p-3 flex flex-col gap-4 transition-all duration-300 hover:shadow-lg">
