64e0cb01614ecb1046d8e0d99037e76a2d154b36
Gabriel Moreira <gabrielmoreira7301@gmail.com>
2025-11-03 10:11:35 +0000

    change

--- src/pages/Leaderboard/[steamid].jsx
+++ src/pages/Leaderboard/[steamid].jsx
@@ -230,13 +230,6 @@ export default function Leaderboard() {
                                             {user.hasGameData ? 'Wasted' : 'No Data'}
                                         </p>
                                     </div>
-
-                                    {/* Click indicator */}
-                                    <div className="ml-4 text-gray-400 opacity-50">
-                                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                                        </svg>
-                                    </div>
                                 </div>
                             ))
                         ) : (
